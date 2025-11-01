3e3b61f18375014b9cd5ba35fabf7e62f0b1e974
AliphaticGuru <sirajmuhd2@gmail.com>
2025-11-01 19:47:06 +0100

    first commit: rustling rusty aliphaticg

---
+++ exercises/02_functions/functions5.rs
@@ -0,0 +1,10 @@
+// TODO: Fix the function body without changing the signature.
+fn square(num: i32) -> i32 {
+    num * num
+}
+
+fn main() {
+    let number = 3;
+    let answer = square(number);
+    println!("The square of {number} is {answer}");
+}
